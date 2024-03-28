c7f43cbed92a280ea88ef8b314529f9a93e5dd56
Ravi-Kumar-Yedla <136640114+Ravi-Kumar-Yedla@users.noreply.github.com>
2024-03-28 10:03:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 //your JS code here. If required.
+const textInput = document.getElementById('text');
+const delayInput = document.getElementById('delay');
+const btn = document.getElementById('btn');
+const output = document.getElementById('output');
+
+//create an asyn fnc to handle the delay 
+async function handleSubmit(e){
+	e.preventDefault();
+	const text = textInput.value;
+	const delay = parseInt(delayInput.value);
+	
+	await delayFunction(delay);
+
+	output.innerText = text;
+}
+
+    const delayFunction =(ms) => new promise((resolve)=>setTimeout(resolve,ms));
+ btn.addEventListener('click',handleSubmit);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
